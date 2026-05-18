d199eb8d9733bc0dc931041b1dc3a146ad241794
asimonok <sashasimonok@gmail.com>
2026-05-18 11:38:07 +0300

    Fix table scrollbar flickering

--- packages/grafana-ui/src/components/Table/TableNG/hooks.ts
+++ packages/grafana-ui/src/components/Table/TableNG/hooks.ts
@@ -463,7 +463,7 @@ export function useColumnResize(
   return dataGridResizeHandler;
 }
 
-export function useScrollbarWidth(ref: RefObject<DataGridHandle | null>, height: number) {
+export function useScrollbarWidth(ref: RefObject<DataGridHandle | null>, width: number, height: number) {
   const [scrollbarWidth, setScrollbarWidth] = useState(0);
 
   useLayoutEffect(() => {
@@ -479,12 +479,8 @@ export function useScrollbarWidth(ref: RefObject<DataGridHandle | null>, height:
 
     updateScrollbarDimensions();
 
-    const resizeObserver = new ResizeObserver(updateScrollbarDimensions);
-    resizeObserver.observe(el);
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [ref, height]);
+    updateScrollbarDimensions();
+  }, [ref, width, height]);
 
   return scrollbarWidth;
 }
